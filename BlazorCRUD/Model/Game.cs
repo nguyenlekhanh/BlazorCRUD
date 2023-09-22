3a2a9091cb542aceb4520df8a68e5d6ee8827180
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-22 18:29:07 -0500

    Add Game Model Show example game to index page

---
+++ BlazorCRUD/Model/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCRUD.Model;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ReleaseDate { get; set; }
+
+}
