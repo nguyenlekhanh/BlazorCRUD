91d872401ab520cf079d6c7be46d053f9bcf0290
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-22 20:28:22 -0500

    add validation

--- BlazorCRUD/Model/Game.cs
+++ BlazorCRUD/Model/Game.cs
@@ -1,11 +1,23 @@
-﻿namespace BlazorCRUD.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCRUD.Model;
 
 public class Game
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public required string Genre { get; set; }
+
+    [Range(1, 100)]
     public decimal Price { get; set; }
+
+
     public DateTime ReleaseDate { get; set; }
 
 }
